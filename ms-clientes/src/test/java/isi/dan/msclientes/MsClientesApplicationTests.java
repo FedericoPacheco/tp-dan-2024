b40a408fb85128805aae69c4bf9be83c3bf9b858
FedericoPacheco <fedepacheco2112@gmail.com>
2024-07-10 16:40:59 -0300

    Hacer funcionar correctamente ms-clientes

--- ms-clientes/src/test/java/isi/dan/msclientes/MsClientesApplicationTests.java
+++ ms-clientes/src/test/java/isi/dan/msclientes/MsClientesApplicationTests.java
@@ -15,8 +15,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
