e2309fdb74cccc2babd92ed4a62bb354246c09b7
FedericoPacheco <fedepacheco2112@gmail.com>
2024-07-17 18:23:25 -0300

    Agregar referencia a JaCoCo

--- ms-productos/src/main/java/isi/dan/ms_productos/MsProductosApplication.java
+++ ms-productos/src/main/java/isi/dan/ms_productos/MsProductosApplication.java
@@ -3,6 +3,16 @@ package isi.dan.ms_productos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * Como usar JaCoCo:
+ * 1) Ejecutar en línea de comandos: 
+ * mvn clean test 
+ * mvn jacoco:report
+ * 2) Instalar la extensión "open in browser"
+ * 3) Buscar el reporte en ./target/site/jacoco y hacer sobre index.html 
+ * click derecho + "Open in default browser"
+ */
+
 @SpringBootApplication
 public class MsProductosApplication {
 
